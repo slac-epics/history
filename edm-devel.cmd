107d634e9e6e472509f4974c2628dd732ef378ef
Bruce Hill <bhill@slac.stanford.edu>
2013-04-08 19:08:47 +0000

    Added 2-2-histViewer.edl for easy viewing of pairs of related signals
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/history/current@12228 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm-devel.cmd
+++ edm-devel.cmd
@@ -4,10 +4,25 @@
 source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
 export SIG=LAS:R52:IOC:49:FilteredPhase
+#edm  -eolc								\
+#-m "SIG=$SIG"							\
+#-m "DUR=LAST_N"							\
+#historyScreens/emb-hist-viewer.edl		\
+#historyScreens/histViewer.edl			\
+#&
+
+export SIG1=LAS:R52:IOC:49:PID:OutputValue
+export SIG2=LAS:R52:IOC:49:PID:ITerm
+export SIG3=LAS:R52:IOC:49:PID:PTerm
+export SIG4=LAS:R52:IOC:49:PID:DTerm
 edm  -eolc								\
--m "SIG=$SIG"							\
+-m "SIG1=$SIG1"							\
+-m "SIG2=$SIG2"							\
+-m "SIG3=$SIG3"							\
+-m "SIG4=$SIG4"							\
 -m "DUR=LAST_N"							\
-historyScreens/emb-hist-viewer.edl		\
-historyScreens/histViewer.edl			\
+-m "TITLE=Development"					\
+historyScreens/emb-2-2-hist-viewer.edl	\
+historyScreens/2-2-histViewer.edl		\
 &
 
