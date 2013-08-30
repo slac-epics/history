ffb963f15ae6f8ff2be6ca6c37bf38f8a07f5020
Bruce Hill <bhill@slac.stanford.edu>
2013-08-30 04:39:45 +0000

    Merged in Garth's changes for compatibility w/ LCLS Naming Czars Added LCLS stats_aliases.template Fixed some PV names in the screens. Disabled scroll bars and tweaked window sizes to avoid having scroll bars eat into the screen on some windows X11 servers. Made aliases for $(PV)_LAST_N_AVG to $(PV)_AVG, etc be automatically generated by the standard substitution files.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/history/current@13757 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm-devel.cmd
+++ edm-devel.cmd
@@ -15,14 +15,23 @@ export SIG1=LAS:R52:IOC:49:PID:OutputValue
 export SIG2=LAS:R52:IOC:49:PID:ITerm
 export SIG3=LAS:R52:IOC:49:PID:PTerm
 export SIG4=LAS:R52:IOC:49:PID:DTerm
-edm  -eolc								\
--m "SIG1=$SIG1"							\
--m "SIG2=$SIG2"							\
--m "SIG3=$SIG3"							\
--m "SIG4=$SIG4"							\
--m "DUR=LAST_N"							\
--m "TITLE=Development"					\
+edm  -eolc										\
+-m "SIG=$SIG1"									\
+-m "SIG1=$SIG1"									\
+-m "SIG2=$SIG2"									\
+-m "SIG3=$SIG3"									\
+-m "SIG4=$SIG4"									\
+-m "DUR=LAST_N"									\
+-m "TITLE=Development"							\
 historyScreens/emb-small-2-2-hist-viewer.edl	\
-historyScreens/small-2-2-histViewer.edl		\
+historyScreens/small-2-2-histViewer.edl			\
+historyScreens/emb-2-2-hist-viewer.edl			\
+historyScreens/2-2-histViewer.edl				\
+historyScreens/emb-2-hist-viewer.edl			\
+historyScreens/2-histViewer.edl					\
+historyScreens/emb-4-hist-viewer.edl			\
+historyScreens/4-histViewer.edl					\
+historyScreens/emb-hist-viewer.edl				\
+historyScreens/histViewer.edl					\
 &
 
